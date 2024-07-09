4ac636a2777ca0d74457e9e74e978647e99cce67
Linet <teweilinet@gmail.com>
2024-07-09 08:33:06 +0300

    cookies and sessions

--- server/app.py
+++ server/app.py
@@ -22,13 +22,53 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    datas=Article.query.all()
+    articles=[]
 
-    pass
+    for data in datas:
+        article={
+            'id':data.id,
+            'author':data.author,
+            'title':data.title,
+            'content':data.content,
+            'preview':data.preview,
+            'minutes_to_read':data.minutes_to_read,
+            'date':data.date,
+            'user_id':data.user_id
+        }
+        articles.append (article)
+    
+    return make_response(jsonify(articles),200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    show_articles=Article.query.filter(Article.id == id).first()
+    return handle_request(show_articles)
+    
+def handle_request(show_articles):
+    if 'page_views' not in session:
+       session['page_views'] =0
 
-    pass
+    session['page_views'] +=1
+
+    if session['page_views'] <=3:
+        response_body={
+            'id':show_articles.id,
+            'author':show_articles.author,
+            'title':show_articles.title,
+            'content':show_articles.content,
+            'preview':show_articles.preview,
+            'minutes_to_read':show_articles.minutes_to_read,
+            'date':show_articles.date,
+            'user_id':show_articles.user_id 
+        }
+        status=200
+        return make_response(jsonify(response_body),status)
+    
+    else: 
+        response_body={'message':'Maximum pageview limit reached'}
+        status=401
+        return make_response(jsonify(response_body),status)
 
 if __name__ == '__main__':
     app.run(port=5555)
